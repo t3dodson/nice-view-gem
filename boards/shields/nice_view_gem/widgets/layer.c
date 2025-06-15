f56eda704468f231f5aff867a8f442e65606f22a
Tom Dodson <t3dodson@gmail.com>
2025-06-14 17:59:55 -0700

    label trim

--- boards/shields/nice_view_gem/widgets/layer.c
+++ boards/shields/nice_view_gem/widgets/layer.c
@@ -2,6 +2,28 @@
 #include "layer.h"
 #include "../assets/custom_fonts.h"
 
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+void trim_layer_suffix(char *str) {
+    size_t len = strlen(str);
+
+    // Walk backwards
+    ssize_t i = len - 1;
+
+    // First skip digits
+    while (i >= 0 && isdigit((unsigned char)str[i])) {
+        i--;
+    }
+
+    // If we find an underscore before the digits, remove it
+    if (i >= 0 && str[i] == '_') {
+        str[i] = '\0';
+    }
+}
+
+
 void draw_layer_status(lv_obj_t *canvas, const struct status_state *state) {
     lv_draw_label_dsc_t label_dsc;
     init_label_dsc(&label_dsc, LVGL_FOREGROUND, &pixel_operator_mono, LV_TEXT_ALIGN_CENTER);
@@ -11,7 +33,7 @@ void draw_layer_status(lv_obj_t *canvas, const struct status_state *state) {
     if (state->layer_label == NULL) {
         sprintf(text, "Layer %i", state->layer_index);
     } else {
-        strncpy(text, state->layer_label, 9);
+        strncpy(text, trim_layer_suffix(state->layer_label), 9);
         to_uppercase(text);
     }
 
